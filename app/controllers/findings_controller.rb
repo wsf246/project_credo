8fa1eaed19f9ff160b7a634a8eabed6182933b01
Brian Kung <brian@callmekung.com>
2014-06-02 22:58:59 -0500

    Housecleaning in findings

--- app/controllers/findings_controller.rb
+++ app/controllers/findings_controller.rb
@@ -4,7 +4,7 @@ class FindingsController < ApplicationController
    @point = Point.find(params[:point])
    @query = Finding.ransack(params[:query]) 
    @findings = @query.result(distinct: true).includes(:research).paginate(page: params[:page])     
-   @researches = Research.find(@findings.map {|a| a.research_id}.uniq)
+   @researches = Research.find(@findings.map(&:research_id).uniq)
   end
 
   def search
