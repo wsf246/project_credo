a085429ad60f798eff50ecb62f0ac61fdb600ab3
wsf246 <willshaofan@gmail.com>
2014-11-12 18:44:41 -0600

    Test jquery fix for chrome with bootstrap require reorder and test alerts

--- app/assets/javascripts/question_form.js
+++ app/assets/javascripts/question_form.js
@@ -1,9 +1,11 @@
-
+$(document).ready(function() {
+  alert("Your book is overdue.");
   $('#question_question_type').on('click', "option[value='Yes/No']", function(){
     $('#question_answers').prop('disabled', true);
+    alert("Your book is overdue!!");
   });
   $('#question_question_type').on('click', "option[value='Multiple Answers']", function(){
     $('#question_answers').prop('disabled', false);
   });
-
+});
 
