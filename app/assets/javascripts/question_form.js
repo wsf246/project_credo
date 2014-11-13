ef307a2c9bda22924836eac21d16e6759605e7b5
wsf246 <willshaofan@gmail.com>
2014-11-12 18:34:16 -0600

    Test jquery compatibility with chrome

--- app/assets/javascripts/question_form.js
+++ app/assets/javascripts/question_form.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+
   $('#question_question_type').on('click', "option[value='Yes/No']", function(){
     $('#question_answers').prop('disabled', true);
   });
   $('#question_question_type').on('click', "option[value='Multiple Answers']", function(){
     $('#question_answers').prop('disabled', false);
   });
-});
+
 
