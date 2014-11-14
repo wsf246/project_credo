07ebfc74eedad0db2e154ef6d444526428604ef9
Brian Kung <brian@callmekung.com>
2014-11-14 13:12:57 -0600

    Adds meta description tag to all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Project Credo"
@@ -10,5 +9,13 @@ module ApplicationHelper
     end
   end
 
+  def meta_description(page_title)
+    base_description = "Find out on Project Credo!"
+    if page_title.empty?
+      tag 'meta', description: base_description
+    else
+      tag 'meta', description: "#{page_title} - #{base_description}"
+    end
+  end
 end
 
