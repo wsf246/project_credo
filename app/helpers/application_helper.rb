dc86ba17e3031426764d39d287bc8341071f00b8
Brian Kung <brian@callmekung.com>
2014-11-14 12:02:56 -0600

    Changes base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "P.C."
+    base_title = "Project Credo"
     if page_title.empty?
       base_title
     else
